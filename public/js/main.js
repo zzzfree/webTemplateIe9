95dbe146b39b93d212b4d97e1f934822139df744
zzzfree <zzzfree@163.com>
2018-08-10 18:23:09 +0800

    unlockconfirm

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 var msb = {
-	check_roles: function (m) {
-		$('.role', $(m) ).each( function(i, v){ 
+	check_roles: function (t) {
+		$('.role', $(t) ).each( function(i, v){ 
 			var r = $(v).attr('role'); 
 			if( _.contains( user.roles, r) ){ 
 				$(v).show();
@@ -12,12 +12,59 @@ var msb = {
 		});
 	}, 
 
-	loadModule: function (t, m) { 
+	loadModule: function (t, m) {  
 		var self = this;
 		$(t).load( 'modules/'+ m + '.html', function(){
-			self.check_roles(m);
+			self.check_roles(t);
 			$(this).append($('<style/>').load('modules/'+ m + '.css')) ;
 			$.getScript('modules/'+ m + '.js'); 
 		} );
-	} 
+	} ,
+
+	component: function (id, cmp, data, cb) { 
+		$.get('modules/components/' + cmp + '.html', function (rsp) { 
+			data = {
+				html: rsp,
+				data: data
+			};
+			$.getScript('modules/components/' + cmp + '.js', function (rsp) {  
+				debugger;
+				o = new __component__();
+				o.init(id, data, cb);
+				cb && cb(o);
+			});
+		});
+	},
+
+	grid: function (target, data, columns, options) {
+		return new Slick.Grid(target, data, columns, options);
+	}, 
+
+	/*
+		load template from script template
+	*/
+	template: function (id, obj) {
+		var tpl = $.trim( document.getElementById(id).innerHTML );
+		Mustache.parse(tpl);
+		return Mustache.render(tpl, obj);
+	},
+
+	/*
+		load template from remote
+	*/
+	templates: function (url, obj, cb) {
+		$.get( 'templates/' + url + '.html' , function (rsp) {  
+			Mustache.parse(rsp);
+			cb && cb( Mustache.render(rsp, obj) );
+		});
+	}
 }; // msb
+
+
+(function ($) {
+    $.urlParam = function (name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]); return null;
+    }
+})(jQuery);
--- public/js/services/user.js
+++ public/js/services/user.js
@@ -1,8 +1,25 @@
 var user = {
 	roles: [],
 	login: function(){
+		// TODO: set roles after login
 		this.roles = [
 			'/mpage/#edit'
 		];
-	}
+	},
+
+	lock: function (id) {
+		alert( 'lock : ' + id );
+	},
+
+	unlock: function () {
+		
+	}, 
+
+	enable: function () {
+		
+	},
+
+	disable: function () {
+		
+	},
 }// user
